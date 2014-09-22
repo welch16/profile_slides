03aeb3eaa818e911677a6ca585853a3310cb08ab
welch16 <welch@stat.wisc.edu>
2014-09-21 21:07:15 -0500

    Added code to generate the first profile matrices

---
+++ rscripts/profile_code.R
@@ -0,0 +1,50 @@
+
+rm(list = ls())
+library(profile)
+source("all_functions.R")
+
+## Load regions
+
+dir = "../data"
+dir1 = file.path(dir,"regions")
+dir2 = file.path(dir,"bam")
+peakfiles = file.path(dir1,list.files(dir1))
+regions = lapply(peakfiles,narrowPeakToGRanges)
+
+regionName = "CTCF-K562"
+fileFormat = "bam"
+maxBandwidth = 301
+bandwidth = 151
+fragLen = 200
+remChr = "chrY"
+windowExt = 2000
+mc.cores = 7
+
+files = list.files(dir2)
+files = file.path(dir2,files[!grepl("bai",files)])
+
+# Set profile object
+ourRegions = regions[[1]]
+start(ourRegions) = regions[[1]]$summit - windowExt
+end(ourRegions) = regions[[1]]$summit + windowExt
+
+ourProfiles = lapply(files,function(x,ourRegions){
+  ourProfile = Profile(regionName,x,fileFormat,maxBandwidth,fragLen,remChr)
+  regions(ourProfile) = ourRegions
+return(ourProfile)},ourRegions)
+
+# Create profile matrices
+ourProfiles = lapply(ourProfiles,function(x)
+  loadReads(x,mc.cores))
+
+ourProfiles = lapply(ourProfiles,function(x)
+  matchReads(x,mc.cores))
+
+ourProfiles = lapply(ourProfiles,function(x)
+  getCoverage(x,mc.cores))
+
+ourProfilesMatrices = lapply(ourProfiles,function(x)
+  ProfileMatrix(x,bandwidth,mc.cores))
+
+save(list = "ourProfileMatrices",file = "../data/generated/matrices.RData")
+
